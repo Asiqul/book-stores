b66d523408253072cdc4692d5a7a2dc276a60998
asiqulh <hakimasiqul37@gmail.com>
2023-08-14 09:28:41 +0700

    fix responsive header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,17 +1,23 @@
+import { HeaderCategory } from './Header-Category';
 import { Hamburger } from './Header-Hamburger';
 import { Logo } from './Header-Logo';
 import { SearchBar } from './Header-Search';
-import { Users } from './Header-Users';
+import { HeaderLogin, Users } from './Header-Users';
 
 export const Header = () => {
   return (
-    <div className="container mx-auto drop-shadow-md inset-2 fixed">
-      <div className="flex justify-between items-center pb-3">
+    <div className="container mx-auto h-32 lg:h-20 lg:py-3 drop-shadow-md border-b-2">
+      <div className="flex justify-between items-center">
         <Hamburger />
         <Logo />
+        <HeaderCategory />
+        <div className="hidden lg:block w-1/2">
+          <SearchBar />
+        </div>
         <Users />
+        <HeaderLogin />
       </div>
-      <div className="container justify-center">
+      <div className="lg:container lg:hidden pt-3">
         <SearchBar />
       </div>
     </div>
