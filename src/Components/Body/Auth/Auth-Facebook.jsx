5f3e4be986bd6812d976d4a5736bafa124105f81
asiqulh <hakimasiqul37@gmail.com>
2023-08-26 23:51:18 +0700

    adding login page, register page, and installing new dependencies react-router-dom

---
+++ src/Components/Body/Auth/Auth-Facebook.jsx
@@ -0,0 +1,30 @@
+import facebook from '../../../assets/icons/facebook.webp';
+
+function AuthFacebook(props) {
+  const { tag, type } = props;
+
+  return (
+    <div className="mt-6">
+      <div className="flex justify-center items-center gap-2">
+        <span className=" w-1/3 h-[1.5px] bg-border opacity-40"></span>
+        <span className="text-sm text-border font-semibold italic">
+          Akun lainnya
+        </span>
+        <span className=" w-1/3 h-[1.5px] bg-border opacity-40"></span>
+      </div>
+      <div className="flex justify-center mt-5">
+        <button
+          type={type}
+          className="btn bg-main border border-second rounded-full font-heading text-lg font-semibold w-full text-main hover:text-second"
+        >
+          <img src={facebook} alt="" className="w-6 bg-transparent" />{' '}
+          <span className="normal-case bg-transparent text-second text-sm ml-2">
+            Masuk dengan akun Facebook
+          </span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default AuthFacebook;
---
+++ src/Pages/Register.jsx
@@ -0,0 +1,39 @@
+import AuthButton from '../Components/Body/Auth/Auth-Button';
+import AuthForms from '../Components/Body/Auth/Auth-Forms';
+import AuthTitle from '../Components/Body/Auth/Auth-Title';
+import Header from '../Fragments/Header';
+
+function Register() {
+  return (
+    <>
+      <Header />
+      <div className="container mx-auto my-4 md:w-1/2 xl:w-1/3">
+        <AuthTitle title="Daftar" />
+        <AuthForms
+          name="fullname"
+          label="Nama Lengkap :"
+          type="text"
+          id="fullname"
+          placeholder="Masukkan nama lengkap"
+        />
+        <AuthForms
+          name="email"
+          label="Email :"
+          type="email"
+          id="email"
+          placeholder="Masukkan email"
+        />
+        <AuthForms
+          name="password"
+          label="Kata sandi :"
+          type="password"
+          id="password"
+          placeholder="Masukkan kata sandi"
+        />
+        <AuthButton tag="Daftar" type="submit" />
+      </div>
+    </>
+  );
+}
+
+export default Register;
